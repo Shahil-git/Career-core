ae6d74bdf92e05ffa3c9d847b45c655c4780d222
Shahil-git <yemtehan80@gmail.com>
2024-05-29 12:30:10 +0600

    first commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-center font-Lobster text-5xl">Welcome to home</h1>
+     
+        </div>
+    );
+};
+
+export default Home;
